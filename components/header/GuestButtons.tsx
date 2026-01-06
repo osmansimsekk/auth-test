810288e25256d0741b5858b9e30e4c87ed2924e8
osman <osmansimsekk54@gmail.com>
2026-01-06 18:46:44 +0300

    Some bug fixed.

--- components/header/GuestButtons.tsx
+++ components/header/GuestButtons.tsx
@@ -13,19 +13,19 @@ export const GuestButtons = ({ variant, onClick }: GuestButtonsProps) => {
   if (variant === "desktop") {
     return (
       <>
-        <Link href="/sign-in">
-          <Button variant="outline" className="gap-2">
+        <Button variant="outline" className="gap-2" asChild>
+          <Link href="/sign-in">
             <User className="h-4 w-4" />
             Giriş Yap
-          </Button>
-        </Link>
+          </Link>
+        </Button>
 
-        <Link href="/sign-up">
-          <Button className="gap-2">
+        <Button className="gap-2" asChild>
+          <Link href="/sign-up">
             <UserPlus className="h-4 w-4" />
             Üye Ol
-          </Button>
-        </Link>
+          </Link>
+        </Button>
       </>
     );
   }
@@ -33,19 +33,19 @@ export const GuestButtons = ({ variant, onClick }: GuestButtonsProps) => {
   // mobile
   return (
     <>
-      <Link href="/sign-in" onClick={onClick}>
-        <Button variant="outline" className="w-full h-11 gap-2">
+      <Button variant="outline" className="w-full h-11 gap-2" asChild>
+        <Link href="/sign-in" onClick={onClick}>
           <User className="h-4 w-4" />
           Giriş Yap
-        </Button>
-      </Link>
+        </Link>
+      </Button>
 
-      <Link href="/sign-up" onClick={onClick}>
-        <Button className="w-full h-11 gap-2">
+      <Button className="w-full h-11 gap-2" asChild>
+        <Link href="/sign-up" onClick={onClick}>
           <UserPlus className="h-4 w-4" />
           Üye Ol
-        </Button>
-      </Link>
+        </Link>
+      </Button>
     </>
   );
 };
