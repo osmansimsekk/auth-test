0ae0161260e1410d777b6a8b8622dbaabbd9db2e
osman <osmansimsekk54@gmail.com>
2026-01-08 00:41:15 +0300

    "BUGS FİXED"

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,17 @@
 import AuthPage from "@/components/page/AuthPage";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const header = await headers();
+  const session = await auth.api.getSession({
+    headers: header,
+  });
+
+  if (session) redirect("/");
+
   return (
     <main className="min-h-screen overflow-visible flex relative">
       {children}
